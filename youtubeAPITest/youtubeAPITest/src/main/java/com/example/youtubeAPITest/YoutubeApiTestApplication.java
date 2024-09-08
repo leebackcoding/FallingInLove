e0ea0799c7fcfaea5e491758ff3bc7ee012caec8
leebackcoding <lcy2825@gmail.com>
2024-09-08 16:24:26 +0900

    youtube API test init

---
+++ youtubeAPITest/youtubeAPITest/src/main/java/com/example/youtubeAPITest/YoutubeApiTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.youtubeAPITest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeApiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeApiTestApplication.class, args);
+	}
+
+}
---
+++ youtubeAPITest/youtubeAPITest/src/test/java/com/example/youtubeAPITest/YoutubeApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.youtubeAPITest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeApiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
